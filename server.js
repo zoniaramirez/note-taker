38a73bff4645c897c0b53ca5ce60a13bdf0a1be7
Zonia Ramirez <jslnramirez@gmail.com>
2024-07-19 10:44:33 -0400

    initialized express app and added middleware

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const uuid = require('uuid');
+
+// Initialize express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
